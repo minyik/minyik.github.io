0cf415783a0aa01c998702343c61cc692a4cf0ee
MIN YI KIM <alsdl7748@naver.com>
2026-06-18 10:48:40 +0900

    포트폴리오 업로드

--- js/main.js
+++ js/main.js
@@ -234,10 +234,25 @@ function initModal() {
     let currentScrollTop = 0;
 
     modalBody.addEventListener('wheel', (e) => {
-        e.stopPropagation();
-        targetScrollTop += e.deltaY;
-        targetScrollTop = Math.max(0, Math.min(targetScrollTop, modalBody.scrollHeight - modalBody.clientHeight));
-    }, { passive: true });
+            e.stopPropagation();
+            targetScrollTop += e.deltaY;
+            targetScrollTop = Math.max(0, Math.min(targetScrollTop, modalBody.scrollHeight - modalBody.clientHeight));
+        }, { passive: true });
+
+        let touchStartY = 0;
+
+        modalBody.addEventListener('touchstart', (e) => {
+            touchStartY = e.touches[0].clientY;
+        }, { passive: true });
+
+        modalBody.addEventListener('touchmove', (e) => {
+            e.stopPropagation();
+            const deltaY = touchStartY - e.touches[0].clientY;
+            touchStartY = e.touches[0].clientY;
+
+            targetScrollTop += deltaY;
+            targetScrollTop = Math.max(0, Math.min(targetScrollTop, modalBody.scrollHeight - modalBody.clientHeight));
+        }, { passive: true });
 
     function smoothModalScroll() {
         currentScrollTop += (targetScrollTop - currentScrollTop) * 0.08;
